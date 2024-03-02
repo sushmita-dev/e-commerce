6bbdbc728ff0fa66e41a97124582e571987de25c
sushmita-dev <sdey7208@gmail.com>
2024-03-02 23:45:35 +0530

    initial changes

---
+++ src/Components/Offers/Offers.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Offers.css'
+import exclusive_image from '../Assets/exclusive_image.png'
+export const Offers = () => {
+  return (
+    <div className='offers'>
+      <div className='offers-left'>
+         <h1>Exlusive</h1>
+         <h1>Offers for you</h1>
+         <p>ONLY ON BEST SELLERS PRODUCT</p>
+         <button>Check Now</button>
+      </div>
+       <div className='offers-right'>
+        <img src={exclusive_image} alt=''/>
+       </div>
+    </div>
+  )
+}
---
+++ src/Pages/ShopCategory.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import { ShopContext } from '../Context/ShopContext'
+
+export const ShopCategory = (props) => {
+  const {all_product} =useContext(ShopContext);
+  return (
+    <div className='shop-category'>
+     
+    </div>
+  )
+}
