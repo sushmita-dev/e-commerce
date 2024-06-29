d94ce26b48118a127fc34970b756354db8ca34fd
sushmita-dev <sdey7208@gmail.com>
2024-06-29 22:17:08 +0530

    'saturday changes'

--- src/Components/ProductDisplay/ProductDisplay.js
+++ src/Components/ProductDisplay/ProductDisplay.js
@@ -20,11 +20,18 @@ const ProductDisplay = (props) => {
       <div className='productdisplay-right'>
         <h1>{product.name}</h1>
         <div className='productdisplay-right-star'></div>
-          <img src='' alt=''/>
-          <img src='' alt=''/>
-          <img src='' alt=''/>
-          <img src='' alt=''/>
-          <img src='' alt=''/>
+          <img src={star_icon} alt=''/>
+          <img src={star_icon} alt=''/>
+          <img src={star_icon} alt=''/>
+          <img src={star_icon} alt=''/>
+          <img src={star_dull_icon} alt=''/>
+          <p>(122)</p>
+      </div>
+       <div className='productdisplay-right-prices'>
+        <div className="productdisplay-right-price-old">${product.old_price}</div>
+        <div className='productdisplay-right-price-new'>${product.new_price}</div>
+       </div>
+      <div className='productdisplay-right-description'>
 
       </div>
     </div>
--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -7,7 +7,7 @@ import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 export const Product = () => {
   const {all_product}=useContext(ShopContext);
   const {productId}=useParams();
-  const product = all_product.find((e)=>e.id === Number(productId))
+  const product = all_product.find((e)=>e.id === Number(productId));
   return (
     <div>
       <Breadcrum product={product}/>
@@ -15,3 +15,4 @@ export const Product = () => {
     </div>
   )
 }
+export default Product
