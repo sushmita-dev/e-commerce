177007b24e1cde5b492475a15ad582da74b3efce
sushmita-dev <sdey7208@gmail.com>
2024-07-03 23:54:03 +0530

    initial changes

--- src/Components/Breadcrums/Breadcrum.js
+++ src/Components/Breadcrums/Breadcrum.js
@@ -5,7 +5,7 @@ export const Breadcrum = (props) => {
     const {product} = props;
   return (
     <div className='breadcrum'> 
-     HOme  <img src={arrow_icon} alt=''/>SHOP<img src={arrow_icon} alt=''/> {product.category} <img src={arrow_icon} alt=''/> {product.name}
+     HOME  <img src={arrow_icon} alt=''/>SHOP<img src={arrow_icon} alt=''/> {product.category} <img src={arrow_icon} alt=''/> {product.name}
     </div>
   )
 }
